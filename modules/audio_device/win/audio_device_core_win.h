b657f13e151a101dea801460780cdc08279816c4
Fela Ameghino <frayx@live.it>
2021-05-05 15:32:40 +0200

    UWP + tgcalls patches

--- modules/audio_device/win/audio_device_core_win.h
+++ modules/audio_device/win/audio_device_core_win.h
@@ -8,43 +8,126 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#ifndef MODULES_AUDIO_DEVICE_WIN_AUDIO_DEVICE_CORE_WIN_H_
-#define MODULES_AUDIO_DEVICE_WIN_AUDIO_DEVICE_CORE_WIN_H_
+#ifndef AUDIO_DEVICE_AUDIO_DEVICE_CORE_WIN_H_
+#define AUDIO_DEVICE_AUDIO_DEVICE_CORE_WIN_H_
 
-#if (_MSC_VER >= 1400)  // only include for VS 2005 and higher
-
-#include "rtc_base/win32.h"
-
-#include "modules/audio_device/audio_device_generic.h"
-
-#include <wmcodecdsp.h>   // CLSID_CWMAudioAEC
-                          // (must be before audioclient.h)
-#include <audioclient.h>  // WASAPI
-#include <audiopolicy.h>
-#include <avrt.h>  // Avrt
+#include <audioclient.h>
 #include <endpointvolume.h>
-#include <mediaobj.h>     // IMediaObject
-#include <mmdeviceapi.h>  // MMDevice
+#include <mmdeviceapi.h>
+#include <windows.foundation.h>
+#include <wrl/client.h>
+#include <wrl/event.h>
+#include <wrl/wrappers/corewrappers.h>
+
+#include <memory>
 
 #include "api/scoped_refptr.h"
+#include "modules/audio_device/audio_device_generic.h"
 #include "rtc_base/synchronization/mutex.h"
-#include "rtc_base/win/scoped_com_initializer.h"
+#include "rtc_base/logging.h"
+#include "rtc_base/win32.h"
 
-// Use Multimedia Class Scheduler Service (MMCSS) to boost the thread priority
-#pragma comment(lib, "avrt.lib")
-// AVRT function pointers
-typedef BOOL(WINAPI* PAvRevertMmThreadCharacteristics)(HANDLE);
-typedef HANDLE(WINAPI* PAvSetMmThreadCharacteristicsA)(LPCSTR, LPDWORD);
-typedef BOOL(WINAPI* PAvSetMmThreadPriority)(HANDLE, AVRT_PRIORITY);
+#define THR(expr)   \
+  do {              \
+    hr = (expr);    \
+    if (FAILED(hr)) \
+      goto Cleanup; \
+  } while (false)
+
+HRESULT WaitForASyncWithEvent(
+    _In_ IAsyncInfo* async_info,
+    _In_ std::shared_ptr<HANDLE> event_completed_handle_ptr,
+    _In_ DWORD timeout_ms);
+
+template <typename T>
+HRESULT WaitForAsyncOperation(
+    _In_ ::ABI::Windows::Foundation::IAsyncOperation<T>* p_async_op) {
+  using ::ABI::Windows::Foundation::AsyncStatus;
+  using ::ABI::Windows::Foundation::IAsyncInfo;
+  using ::ABI::Windows::Foundation::IAsyncOperation;
+  using ::ABI::Windows::Foundation::IAsyncOperationCompletedHandler;
+  using ::Microsoft::WRL::Callback;
+  using ::Microsoft::WRL::Wrappers::Event;
+
+  HRESULT hr = S_OK;
+  HRESULT hr_async_error = S_OK;
+  const DWORD timeout_ms = 180000;
+  ComPtr<IAsyncOperation<T>> async_op(p_async_op);
+  ComPtr<IAsyncInfo> async_info;
+  std::shared_ptr<HANDLE> event_completed_handle_ptr = NULL;
+
+  APTTYPE apt_type;
+  APTTYPEQUALIFIER apt_qualifier;
+  hr = CoGetApartmentType(&apt_type, &apt_qualifier);
+  // Please make the caller of this API run on a MTA appartment type.
+  // The caller shouldn't be running on the UI thread (STA).
+  if (SUCCEEDED(hr) && (apt_type != APTTYPE_MTA)) {
+    RTC_LOG(LS_ERROR) << "Waiting in a non-MTA thread. Deadlocks might occur.";
+  }
+
+  // IAsyncInfo::get_Status is needed to check if operation started and if any
+  // errors happened.
+  if (SUCCEEDED(hr)) {
+    hr = async_op.template As<IAsyncInfo>(&async_info);
+  }
+
+  // Creates the Event to be used to block and suspend until the async
+  // operation finishes.
+  if (SUCCEEDED(hr)) {
+    // Using raw HANDLE because WRL::Wrappers::Event doesn't always work across
+    // threads and there is no agile option.
+    HANDLE event_completed_handle =
+        ::CreateEventEx(nullptr, nullptr, CREATE_EVENT_MANUAL_RESET,
+                        WRITE_OWNER | EVENT_ALL_ACCESS);
+    if (event_completed_handle) {
+      hr = S_OK;
+      event_completed_handle_ptr = std::shared_ptr<HANDLE>(
+          new HANDLE{event_completed_handle}, [](HANDLE* h) {
+            ::CloseHandle(*h);
+            delete h;
+          });
+    } else {
+      hr = HRESULT_FROM_WIN32(GetLastError());
+    }
+  }
+
+  if (SUCCEEDED(hr)) {
+    hr = GetLastError() == ERROR_ALREADY_EXISTS
+             ? HRESULT_FROM_WIN32(ERROR_ALREADY_EXISTS)
+             : S_OK;
+  }
+
+  if (SUCCEEDED(hr)) {
+    // Defines the callback that will signal the event to unblock and resume.
+    hr = async_op->put_Completed(
+        Callback<IAsyncOperationCompletedHandler<T>>(
+            [event_completed_handle_ptr](IAsyncOperation<T>*,
+                                         AsyncStatus async_status) -> HRESULT {
+              ::SetEvent(*event_completed_handle_ptr);
+
+              return async_status == Completed ? S_OK : E_ABORT;
+            })
+            .Get());
+  }
+
+  // Block and suspend thread until the async operation finishes or timeout.
+  if (SUCCEEDED(hr)) {
+    hr = WaitForASyncWithEvent(async_info.Get(), event_completed_handle_ptr,
+                               timeout_ms);
+  }
+
+  if (SUCCEEDED(hr)) {
+    hr = async_info->get_ErrorCode(&hr_async_error);
+  }
+
+  if (SUCCEEDED(hr)) {
+    hr = hr_async_error;
+  }
+
+  return hr;
+}
 
 namespace webrtc {
-
-const float MAX_CORE_SPEAKER_VOLUME = 255.0f;
-const float MIN_CORE_SPEAKER_VOLUME = 0.0f;
-const float MAX_CORE_MICROPHONE_VOLUME = 255.0f;
-const float MIN_CORE_MICROPHONE_VOLUME = 0.0f;
-const uint16_t CORE_SPEAKER_VOLUME_STEP_SIZE = 1;
-const uint16_t CORE_MICROPHONE_VOLUME_STEP_SIZE = 1;
 
 class AudioDeviceWindowsCore : public AudioDeviceGeneric {
  public:
@@ -57,244 +140,95 @@ class AudioDeviceWindowsCore : public AudioDeviceGeneric {
   virtual int32_t ActiveAudioLayer(
       AudioDeviceModule::AudioLayer& audioLayer) const;
 
+  // Attach audio device buffer
+  virtual void AttachAudioBuffer(AudioDeviceBuffer* audioBuffer);
+
   // Main initializaton and termination
-  virtual InitStatus Init() RTC_LOCKS_EXCLUDED(mutex_);
-  virtual int32_t Terminate() RTC_LOCKS_EXCLUDED(mutex_);
+  virtual InitStatus Init();
+  virtual int32_t Terminate();
   virtual bool Initialized() const;
 
   // Device enumeration
-  virtual int16_t PlayoutDevices() RTC_LOCKS_EXCLUDED(mutex_);
-  virtual int16_t RecordingDevices() RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int16_t PlayoutDevices();
+  virtual int16_t RecordingDevices();
   virtual int32_t PlayoutDeviceName(uint16_t index,
                                     char name[kAdmMaxDeviceNameSize],
-                                    char guid[kAdmMaxGuidSize])
-      RTC_LOCKS_EXCLUDED(mutex_);
+                                    char guid[kAdmMaxGuidSize]);
   virtual int32_t RecordingDeviceName(uint16_t index,
                                       char name[kAdmMaxDeviceNameSize],
-                                      char guid[kAdmMaxGuidSize])
-      RTC_LOCKS_EXCLUDED(mutex_);
+                                      char guid[kAdmMaxGuidSize]);
 
   // Device selection
-  virtual int32_t SetPlayoutDevice(uint16_t index) RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t SetPlayoutDevice(uint16_t index);
   virtual int32_t SetPlayoutDevice(AudioDeviceModule::WindowsDeviceType device);
-  virtual int32_t SetRecordingDevice(uint16_t index) RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t SetRecordingDevice(uint16_t index);
   virtual int32_t SetRecordingDevice(
-      AudioDeviceModule::WindowsDeviceType device) RTC_LOCKS_EXCLUDED(mutex_);
+      AudioDeviceModule::WindowsDeviceType device);
 
   // Audio transport initialization
   virtual int32_t PlayoutIsAvailable(bool& available);
-  virtual int32_t InitPlayout() RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t InitPlayout();
   virtual bool PlayoutIsInitialized() const;
   virtual int32_t RecordingIsAvailable(bool& available);
-  virtual int32_t InitRecording() RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t InitRecording();
   virtual bool RecordingIsInitialized() const;
 
   // Audio transport control
-  virtual int32_t StartPlayout() RTC_LOCKS_EXCLUDED(mutex_);
-  virtual int32_t StopPlayout() RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t StartPlayout();
+  virtual int32_t StopPlayout();
   virtual bool Playing() const;
-  virtual int32_t StartRecording() RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t StartRecording();
   virtual int32_t StopRecording();
   virtual bool Recording() const;
 
   // Audio mixer initialization
-  virtual int32_t InitSpeaker() RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t InitSpeaker();
   virtual bool SpeakerIsInitialized() const;
-  virtual int32_t InitMicrophone() RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t InitMicrophone();
   virtual bool MicrophoneIsInitialized() const;
 
   // Speaker volume controls
-  virtual int32_t SpeakerVolumeIsAvailable(bool& available)
-      RTC_LOCKS_EXCLUDED(mutex_);
-  virtual int32_t SetSpeakerVolume(uint32_t volume) RTC_LOCKS_EXCLUDED(mutex_);
-  virtual int32_t SpeakerVolume(uint32_t& volume) const
-      RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t SpeakerVolumeIsAvailable(bool& available);
+  virtual int32_t SetSpeakerVolume(uint32_t volume);
+  virtual int32_t SpeakerVolume(uint32_t& volume) const;
   virtual int32_t MaxSpeakerVolume(uint32_t& maxVolume) const;
   virtual int32_t MinSpeakerVolume(uint32_t& minVolume) const;
 
   // Microphone volume controls
-  virtual int32_t MicrophoneVolumeIsAvailable(bool& available)
-      RTC_LOCKS_EXCLUDED(mutex_);
-  virtual int32_t SetMicrophoneVolume(uint32_t volume)
-      RTC_LOCKS_EXCLUDED(mutex_, volume_mutex_);
-  virtual int32_t MicrophoneVolume(uint32_t& volume) const
-      RTC_LOCKS_EXCLUDED(mutex_, volume_mutex_);
+  virtual int32_t MicrophoneVolumeIsAvailable(bool& available);
+  virtual int32_t SetMicrophoneVolume(uint32_t volume);
+  virtual int32_t MicrophoneVolume(uint32_t& volume) const;
   virtual int32_t MaxMicrophoneVolume(uint32_t& maxVolume) const;
   virtual int32_t MinMicrophoneVolume(uint32_t& minVolume) const;
 
   // Speaker mute control
-  virtual int32_t SpeakerMuteIsAvailable(bool& available)
-      RTC_LOCKS_EXCLUDED(mutex_);
-  virtual int32_t SetSpeakerMute(bool enable) RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t SpeakerMuteIsAvailable(bool& available);
+  virtual int32_t SetSpeakerMute(bool enable);
   virtual int32_t SpeakerMute(bool& enabled) const;
 
   // Microphone mute control
-  virtual int32_t MicrophoneMuteIsAvailable(bool& available)
-      RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t MicrophoneMuteIsAvailable(bool& available);
   virtual int32_t SetMicrophoneMute(bool enable);
   virtual int32_t MicrophoneMute(bool& enabled) const;
 
   // Stereo support
   virtual int32_t StereoPlayoutIsAvailable(bool& available);
-  virtual int32_t SetStereoPlayout(bool enable) RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t SetStereoPlayout(bool enable);
   virtual int32_t StereoPlayout(bool& enabled) const;
   virtual int32_t StereoRecordingIsAvailable(bool& available);
-  virtual int32_t SetStereoRecording(bool enable) RTC_LOCKS_EXCLUDED(mutex_);
-  virtual int32_t StereoRecording(bool& enabled) const
-      RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t SetStereoRecording(bool enable);
+  virtual int32_t StereoRecording(bool& enabled) const;
 
   // Delay information and control
-  virtual int32_t PlayoutDelay(uint16_t& delayMS) const
-      RTC_LOCKS_EXCLUDED(mutex_);
+  virtual int32_t PlayoutDelay(uint16_t& delayMS) const;
 
+  // AEC support
   virtual bool BuiltInAECIsAvailable() const;
-
   virtual int32_t EnableBuiltInAEC(bool enable);
 
- public:
-  virtual void AttachAudioBuffer(AudioDeviceBuffer* audioBuffer);
-
  private:
-  bool KeyPressed() const;
-
- private:  // avrt function pointers
-  PAvRevertMmThreadCharacteristics _PAvRevertMmThreadCharacteristics;
-  PAvSetMmThreadCharacteristicsA _PAvSetMmThreadCharacteristicsA;
-  PAvSetMmThreadPriority _PAvSetMmThreadPriority;
-  HMODULE _avrtLibrary;
-  bool _winSupportAvrt;
-
- private:  // thread functions
-  int32_t InitSpeakerLocked() RTC_EXCLUSIVE_LOCKS_REQUIRED(mutex_);
-  int32_t InitMicrophoneLocked() RTC_EXCLUSIVE_LOCKS_REQUIRED(mutex_);
-  int16_t PlayoutDevicesLocked() RTC_EXCLUSIVE_LOCKS_REQUIRED(mutex_);
-  int16_t RecordingDevicesLocked() RTC_EXCLUSIVE_LOCKS_REQUIRED(mutex_);
-  DWORD InitCaptureThreadPriority();
-  void RevertCaptureThreadPriority();
-  static DWORD WINAPI WSAPICaptureThread(LPVOID context);
-  DWORD DoCaptureThread();
-
-  static DWORD WINAPI WSAPICaptureThreadPollDMO(LPVOID context);
-  DWORD DoCaptureThreadPollDMO() RTC_LOCKS_EXCLUDED(mutex_);
-
-  static DWORD WINAPI WSAPIRenderThread(LPVOID context);
-  DWORD DoRenderThread();
-
-  void _Lock();
-  void _UnLock();
-
-  int SetDMOProperties();
-
-  int SetBoolProperty(IPropertyStore* ptrPS,
-                      REFPROPERTYKEY key,
-                      VARIANT_BOOL value);
-
-  int SetVtI4Property(IPropertyStore* ptrPS, REFPROPERTYKEY key, LONG value);
-
-  int32_t _EnumerateEndpointDevicesAll(EDataFlow dataFlow) const;
-  void _TraceCOMError(HRESULT hr) const;
-
-  int32_t _RefreshDeviceList(EDataFlow dir);
-  int16_t _DeviceListCount(EDataFlow dir);
-  int32_t _GetDefaultDeviceName(EDataFlow dir,
-                                ERole role,
-                                LPWSTR szBuffer,
-                                int bufferLen);
-  int32_t _GetListDeviceName(EDataFlow dir,
-                             int index,
-                             LPWSTR szBuffer,
-                             int bufferLen);
-  int32_t _GetDeviceName(IMMDevice* pDevice, LPWSTR pszBuffer, int bufferLen);
-  int32_t _GetListDeviceID(EDataFlow dir,
-                           int index,
-                           LPWSTR szBuffer,
-                           int bufferLen);
-  int32_t _GetDefaultDeviceID(EDataFlow dir,
-                              ERole role,
-                              LPWSTR szBuffer,
-                              int bufferLen);
-  int32_t _GetDefaultDeviceIndex(EDataFlow dir, ERole role, int* index);
-  int32_t _GetDeviceID(IMMDevice* pDevice, LPWSTR pszBuffer, int bufferLen);
-  int32_t _GetDefaultDevice(EDataFlow dir, ERole role, IMMDevice** ppDevice);
-  int32_t _GetListDevice(EDataFlow dir, int index, IMMDevice** ppDevice);
-
-  int32_t InitRecordingDMO();
-
-  ScopedCOMInitializer _comInit;
-  AudioDeviceBuffer* _ptrAudioBuffer;
-  mutable Mutex mutex_;
-  mutable Mutex volume_mutex_ RTC_ACQUIRED_AFTER(mutex_);
-
-  IMMDeviceEnumerator* _ptrEnumerator;
-  IMMDeviceCollection* _ptrRenderCollection;
-  IMMDeviceCollection* _ptrCaptureCollection;
-  IMMDevice* _ptrDeviceOut;
-  IMMDevice* _ptrDeviceIn;
-
-  IAudioClient* _ptrClientOut;
-  IAudioClient* _ptrClientIn;
-  IAudioRenderClient* _ptrRenderClient;
-  IAudioCaptureClient* _ptrCaptureClient;
-  IAudioEndpointVolume* _ptrCaptureVolume;
-  ISimpleAudioVolume* _ptrRenderSimpleVolume;
-
-  // DirectX Media Object (DMO) for the built-in AEC.
-  rtc::scoped_refptr<IMediaObject> _dmo;
-  rtc::scoped_refptr<IMediaBuffer> _mediaBuffer;
-  bool _builtInAecEnabled;
-
-  HANDLE _hRenderSamplesReadyEvent;
-  HANDLE _hPlayThread;
-  HANDLE _hRenderStartedEvent;
-  HANDLE _hShutdownRenderEvent;
-
-  HANDLE _hCaptureSamplesReadyEvent;
-  HANDLE _hRecThread;
-  HANDLE _hCaptureStartedEvent;
-  HANDLE _hShutdownCaptureEvent;
-
-  HANDLE _hMmTask;
-
-  UINT _playAudioFrameSize;
-  uint32_t _playSampleRate;
-  uint32_t _devicePlaySampleRate;
-  uint32_t _playBlockSize;
-  uint32_t _devicePlayBlockSize;
-  uint32_t _playChannels;
-  uint32_t _sndCardPlayDelay;
-  UINT64 _writtenSamples;
-  UINT64 _readSamples;
-
-  UINT _recAudioFrameSize;
-  uint32_t _recSampleRate;
-  uint32_t _recBlockSize;
-  uint32_t _recChannels;
-
-  uint16_t _recChannelsPrioList[3];
-  uint16_t _playChannelsPrioList[2];
-
-  LARGE_INTEGER _perfCounterFreq;
-  double _perfCounterFactor;
-
- private:
-  bool _initialized;
-  bool _recording;
-  bool _playing;
-  bool _recIsInitialized;
-  bool _playIsInitialized;
-  bool _speakerIsInitialized;
-  bool _microphoneIsInitialized;
-
-  bool _usingInputDeviceIndex;
-  bool _usingOutputDeviceIndex;
-  AudioDeviceModule::WindowsDeviceType _inputDevice;
-  AudioDeviceModule::WindowsDeviceType _outputDevice;
-  uint16_t _inputDeviceIndex;
-  uint16_t _outputDeviceIndex;
+  struct AudioDeviceWindowsCoreInternal* _internal;
 };
-
-#endif  // #if (_MSC_VER >= 1400)
-
 }  // namespace webrtc
 
-#endif  // MODULES_AUDIO_DEVICE_WIN_AUDIO_DEVICE_CORE_WIN_H_
+#endif  // AUDIO_DEVICE_AUDIO_DEVICE_CORE_WIN_H_
--- modules/video_capture/video_capture_defines.h
+++ modules/video_capture/video_capture_defines.h
@@ -11,6 +11,10 @@
 #ifndef MODULES_VIDEO_CAPTURE_VIDEO_CAPTURE_DEFINES_H_
 #define MODULES_VIDEO_CAPTURE_VIDEO_CAPTURE_DEFINES_H_
 
+#if defined(WEBRTC_VIDEO_CAPTURE_WINRT)
+#include <wrl/client.h>
+#endif  // WEBRTC_VIDEO_CAPTURE_WINRT
+
 #include "api/video/video_frame.h"
 #include "common_video/libyuv/include/webrtc_libyuv.h"
 
@@ -28,14 +32,25 @@ struct VideoCaptureCapability {
   int32_t maxFPS;
   VideoType videoType;
   bool interlaced;
+#if defined(WEBRTC_VIDEO_CAPTURE_WINRT)
+  std::wstring profile_id;
+  ::Microsoft::WRL::ComPtr<::IUnknown> media_capture_video_profile;
+  ::Microsoft::WRL::ComPtr<::IUnknown> record_media_description;
+#endif  // WEBRTC_VIDEO_CAPTURE_WINRT
 
-  VideoCaptureCapability() {
-    width = 0;
-    height = 0;
-    maxFPS = 0;
-    videoType = VideoType::kUnknown;
-    interlaced = false;
-  }
+  VideoCaptureCapability()
+      : width(0),
+        height(0),
+        maxFPS(0),
+        videoType(VideoType::kUnknown),
+        interlaced(false)
+#if defined(WEBRTC_VIDEO_CAPTURE_WINRT)
+        , profile_id(L"")
+        , media_capture_video_profile(nullptr)
+        , record_media_description(nullptr)
+#endif  // WEBRTC_VIDEO_CAPTURE_WINRT
+  {}
+
   bool operator!=(const VideoCaptureCapability& other) const {
     if (width != other.width)
       return true;
@@ -47,6 +62,16 @@ struct VideoCaptureCapability {
       return true;
     if (interlaced != other.interlaced)
       return true;
+#if defined(WEBRTC_VIDEO_CAPTURE_WINRT)
+    if (profile_id != other.profile_id)
+      return true;
+    if (media_capture_video_profile.Get() !=
+        other.media_capture_video_profile.Get())
+      return true;
+    if (record_media_description.Get() != other.record_media_description.Get())
+      return true;
+#endif  // WEBRTC_VIDEO_CAPTURE_WINRT
+
     return false;
   }
   bool operator==(const VideoCaptureCapability& other) const {
